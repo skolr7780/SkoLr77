1f541149933e4a988eb6e24b75843828a33edd1b
skolr7780 <45235249+skolr7780@users.noreply.github.com>
2020-09-03 23:30:23 +0300

    a33

--- js/script.js
+++ js/script.js
@@ -57,14 +57,3 @@ setTimeout(function(){
     });
 }, 8000);
 
-setTimeout(function(){
-    $(".element5").css("display", "inherit");
-    $(".element5").typed({
-        strings: ["Devloper By SkOlr77"],
-        typeSpeed: 30, // typing speed
-        backDelay: 750, // pause before backspacing
-        loop: false, // loop on or off (true or false)
-        loopCount: false, // number of loops, false = infinite
-        callback: function(){ } // call function after typing is done
-    });
-}, 8000);
